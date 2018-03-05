dd8aea5b5f56fd53e66083ca1a822d9fc8329c89
alex <alexe_webmail@yahoo.com>
2018-03-05 09:33:01 -0500

    Update header block date for all DG-Sim files

--- Runtime/DGSimTransformer.Output.vb
+++ Runtime/DGSimTransformer.Output.vb
@@ -1,7 +1,7 @@
 ﻿'*********************************************************************************************
 ' DG-Sim: A SyncroSim Module for simulating demographic population models.
 '
-' Copyright © 2007-2017 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
+' Copyright © 2007-2018 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 '
 '*********************************************************************************************
 
