15a785b3069417b90ea2a161a1b9d93a19b5c327
alex <alexe_webmail@yahoo.com>
2018-10-23 11:32:36 -0700

    Update assembly and modules for version 2.0.34

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.1.30.0")>
-<Assembly: AssemblyFileVersion("2.1.30.0")>
+<Assembly: AssemblyVersion("2.1.31.0")>
+<Assembly: AssemblyFileVersion("2.1.31.0")>
 <Assembly: NeutralResourcesLanguage("en-US")>
