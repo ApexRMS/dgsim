be9c4223d16d61c4fc2833b73066da9909180928
alex <alexe_webmail@yahoo.com>
2018-10-29 17:18:35 -0700

    Update the header blocks for all DG-Sim files

--- Runtime/SummaryOutputRecruits.vb
+++ Runtime/SummaryOutputRecruits.vb
@@ -1,5 +1,5 @@
 ﻿'*********************************************************************************************
-' DG-Sim: A SyncroSim Module for simulating demographics of wildlife populations.
+' DG-Sim: A SyncroSim Package for simulating demographics of wildlife populations.
 '
 ' Copyright © 2007-2018 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 '
