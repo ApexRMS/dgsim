d2a1714d9ae4f539fbe2d4283c88b593c4d4085a
alex <alexe_webmail@yahoo.com>
2018-12-05 12:45:15 -0800

    Update version numbers for release 2.0.38

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.1.34.0")>
-<Assembly: AssemblyFileVersion("2.1.34.0")>
+<Assembly: AssemblyVersion("2.1.35.0")>
+<Assembly: AssemblyFileVersion("2.1.35.0")>
 <Assembly: NeutralResourcesLanguage("en-US")>
