0bed70a83284436375c2b6edf089aef4a37f98bc
alex <alexe_webmail@yahoo.com>
2019-02-01 16:47:13 -0800

    Update file header blocks to match readme.md.

--- src/DataFeedViews/InitialPopulationDataFeedView.vb
+++ src/DataFeedViews/InitialPopulationDataFeedView.vb
@@ -1,5 +1,5 @@
 ﻿'*********************************************************************************************
-' DG-Sim: A SyncroSim Package for simulating demographics of wildlife populations.
+' dgsim: SyncroSim Base Package for simulating demographics of wildlife populations.
 '
 ' Copyright © 2007-2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 '
