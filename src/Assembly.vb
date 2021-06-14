98a10124f42234c690a2b8d884a1f7ce97b7ac9b
Katie Birchard <birchardkatie@gmail.com>
2021-06-14 15:13:41 -0700

    Updating copyright headers

--- src/Assembly.vb
+++ src/Assembly.vb
@@ -1,7 +1,7 @@
 ﻿'***********************************************************************************************
 ' dgsim: SyncroSim Base Package for simulating demographics of wildlife populations.
 '
-' Copyright © 2007-2019 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.
+' Copyright © 2007-2021 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.
 '
 '***********************************************************************************************
 
